390227a1ec84b775db60094cd7e9bedeff80ce6c
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-05-26 13:14:28 +0100

    First commit on Tue May 26 13:14:02 HGMT 2015

---
+++ app/src/main/java/com/tfg/sawan/bsecure/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.tfg.sawan.bsecure;
+
+import android.app.Activity;
+
+/**
+ * File name:	com.tfg.sawan.bsecure
+ * Version:		1.0
+ * Date:		26/05/2015 12:39
+ * Author:		Sawan J. Kapai Harpalani
+ * Copyright:	Copyright 200X Sawan J. Kapai Harpalani
+ * <p/>
+ * This file is part of Bsecure.
+ * <p/>
+ * Bsecure is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software
+ * Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * <p/>
+ * Bsecure is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General
+ * Public License along with Bsecure. If not, see
+ * http://www.gnu.org/licenses/.
+ */
+
+
+public class SplashScreen extends Activity {
+
+}
