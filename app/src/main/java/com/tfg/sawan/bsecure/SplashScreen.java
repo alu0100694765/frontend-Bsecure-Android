b2b5d5fd124c902fd6e205744f678788426c5a5e
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-05-26 15:51:44 +0100

    Splash screen working ^^

--- app/src/main/java/com/tfg/sawan/bsecure/SplashScreen.java
+++ app/src/main/java/com/tfg/sawan/bsecure/SplashScreen.java
@@ -1,6 +1,9 @@
 package com.tfg.sawan.bsecure;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * File name:	com.tfg.sawan.bsecure
@@ -30,5 +33,29 @@ import android.app.Activity;
 
 
 public class SplashScreen extends Activity {
+    // Splash screen timer
+    private final static int SPLASH_TIME_OUT = 3000;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+            * Showing splash screen with a timer.
+            */
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent main_activity = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(main_activity);
+
+                // close the activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
 }
