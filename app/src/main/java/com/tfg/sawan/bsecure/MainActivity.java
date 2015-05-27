eb503526c31a3b1b4dc6f0f17c32acc641073e18
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-05-27 16:24:31 +0100

    Starting again

--- app/src/main/java/com/tfg/sawan/bsecure/MainActivity.java
+++ app/src/main/java/com/tfg/sawan/bsecure/MainActivity.java
@@ -1,9 +1,34 @@
 package com.tfg.sawan.bsecure;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * File name:	com.tfg.sawan.bsecure
+ * Version:		1.0
+ * Date:		26/05/2015 12:39
+ * Author:		Sawan J. Kapai Harpalani
+ * Copyright:	Copyright 200X Sawan J. Kapai Harpalani
+ * <p/>
+ * This file is part of Bsecure.
+ * <p/>
+ * Bsecure is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software
+ * Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * <p/>
+ * Bsecure is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General
+ * Public License along with Bsecure. If not, see
+ * http://www.gnu.org/licenses/.
+ */
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,27 +37,5 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
