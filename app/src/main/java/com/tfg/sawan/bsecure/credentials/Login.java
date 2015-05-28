2841996e892d9fb0ec195feb3d866538bb92451d
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-05-28 11:28:37 +0100

    Added Internet permission

--- app/src/main/java/com/tfg/sawan/bsecure/credentials/Login.java
+++ app/src/main/java/com/tfg/sawan/bsecure/credentials/Login.java
@@ -1,5 +1,6 @@
 package com.tfg.sawan.bsecure.credentials;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,33 +9,11 @@ import android.view.MenuItem;
 import com.tfg.sawan.bsecure.R;
 
 
-public class Login extends ActionBarActivity {
+public class Login extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
