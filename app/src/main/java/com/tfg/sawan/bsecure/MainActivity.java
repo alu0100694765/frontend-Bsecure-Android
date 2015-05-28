ad3c28096ee61f616dfbbec63017246dea0bf646
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-05-28 11:25:23 +0100

    Changed layout of main activity

--- app/src/main/java/com/tfg/sawan/bsecure/MainActivity.java
+++ app/src/main/java/com/tfg/sawan/bsecure/MainActivity.java
@@ -37,6 +37,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
