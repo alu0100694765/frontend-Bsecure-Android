1f7c449da3dc504a46534ed962edd9a188724f27
Sawan J. Kapai Harpalani <alu0100694765@ull.edu.es>
2015-05-30 00:32:06 +0100

    Implementing expiry check system tomorrow will be a new day time to :sleeping:

--- app/src/main/java/com/tfg/sawan/bsecure/credentials/Token.java
+++ app/src/main/java/com/tfg/sawan/bsecure/credentials/Token.java
@@ -1,5 +1,10 @@
 package com.tfg.sawan.bsecure.credentials;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by Sawan on 30/05/2015.
  */
@@ -21,5 +26,23 @@ public class Token {
 
     public static void setExpiry_date(String expiry_date) {
         Token.expiry_date = expiry_date;
+    }
+
+    public static boolean isExpired() {
+        SimpleDateFormat date_format = new SimpleDateFormat("MM/dd/yyyy");
+        Date expiry_date_token = new Date();
+        Calendar actual_date = Calendar.getInstance();
+
+        try {
+            expiry_date_token = date_format.parse(Token.expiry_date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        if (!expiry_date_token.before(actual_date)) {
+
+        }
+
+        return false;
     }
 }
