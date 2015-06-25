8c0106742f6d8e3b496ce055fc2067334238a0c4
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-06-25 19:40:01 +0100

    Fixed bug of login with empty strings

--- app/src/main/java/com/tfg/sawan/bsecure/credentials/Login.java
+++ app/src/main/java/com/tfg/sawan/bsecure/credentials/Login.java
@@ -2,6 +2,8 @@ package com.tfg.sawan.bsecure.credentials;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.StrictMode;
@@ -43,6 +45,9 @@ public class Login extends Activity {
      */
     protected Button login_button;
 
+    /**
+     * SignUp button
+     */
     protected Button signup_button;
 
     /**
@@ -55,7 +60,12 @@ public class Login extends Activity {
      */
     protected String password;
 
+    /**
+     * Static Messages
+     */
+    protected static final String FILL_FIELD = "Fill this field";
     protected static final String INVALID_CREDENTIALS_MESSAGE = "Invalid credentials";
+    protected static final String EMPTY_FIELDS = "Some fields are incomplete! Fill them!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,73 +113,91 @@ public class Login extends Activity {
         EditText password_editText = (EditText) findViewById(R.id.password);
         password = password_editText.getText().toString();
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+        // If editText are not empty
+        if (!username.trim().equals("") && !password.trim().equals("")) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
 
-        // Create client
-        HttpClient client = new DefaultHttpClient();
+            // Create client
+            HttpClient client = new DefaultHttpClient();
 
-        // Create Post object
-        HttpPost post = new HttpPost("http://bsecure-sawankapai.rhcloud.com/login-android");
+            // Create Post object
+            HttpPost post = new HttpPost("http://bsecure-sawankapai.rhcloud.com/login-android");
 
-        // Add post parameters
-        ArrayList<NameValuePair> parameters = new ArrayList<NameValuePair>();
-        parameters.add(new BasicNameValuePair("username", username));
-        parameters.add(new BasicNameValuePair("password", password));
+            // Add post parameters
+            ArrayList<NameValuePair> parameters = new ArrayList<NameValuePair>();
+            parameters.add(new BasicNameValuePair("username", username));
+            parameters.add(new BasicNameValuePair("password", password));
 
-        // Encode data
-        try {
-            post.setEntity(new UrlEncodedFormEntity(parameters));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-
-        // Make the HTTP POST request
-        HttpResponse response = null;
-        try {
-            response = client.execute(post);
-            Log.d("Response", response.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Get token
-        String token = null;
-        String expiry_date = null;
-        String name = null;
-        if (response.getStatusLine().getStatusCode() == 200) {
-            HttpEntity entity = response.getEntity();
-            JSONObject json = null;
+            // Encode data
             try {
-                json = new JSONObject(EntityUtils.toString(entity));
-                token = json.getString("token");
-                expiry_date = json.getString("exp").replace("/", "-");
-                name = json.getString("name");
-            } catch (JSONException e) {
+                post.setEntity(new UrlEncodedFormEntity(parameters));
+            } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
-        } else {
-            Toast.makeText(this, INVALID_CREDENTIALS_MESSAGE, Toast.LENGTH_SHORT).show();
-            Intent restart = getIntent();
+
+            // Make the HTTP POST request
+            HttpResponse response = null;
+            try {
+                response = client.execute(post);
+                Log.d("Response", response.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Get token
+            String token = null;
+            String expiry_date = null;
+            String name = null;
+            if (response.getStatusLine().getStatusCode() == 200) {
+                HttpEntity entity = response.getEntity();
+                JSONObject json = null;
+                try {
+                    json = new JSONObject(EntityUtils.toString(entity));
+                    token = json.getString("token");
+                    expiry_date = json.getString("exp").replace("/", "-");
+                    name = json.getString("name");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(this, INVALID_CREDENTIALS_MESSAGE, Toast.LENGTH_SHORT).show();
+                Intent restart = getIntent();
+                finish();
+                startActivity(restart);
+            }
+
+            // Add to sharedPreferences
+            Preferences.savePreferences(this, "token", token);
+            Preferences.savePreferences(this, "expiry", expiry_date);
+            Preferences.savePreferences(this, "name", name);
+
+            // Set the Token
+            Token.setToken(token);
+            Token.setExpiry_date(expiry_date);
+            Token.setUser_name(name);
+
+            // Switch to main activity
+            Intent main_activity = new Intent(Login.this, MainActivity.class);
+            startActivity(main_activity);
+
+            // Finish the activity
             finish();
-            startActivity(restart);
+
+          // Else alert user changing placeholder and Toast
+        } else {
+
+            if (username.trim().equals("")) {
+                user_name_editText.setHintTextColor(ColorStateList.valueOf(Color.RED));
+                user_name_editText.setHint(FILL_FIELD);
+            }
+
+            if (password.trim().equals("")) {
+                password_editText.setHintTextColor(ColorStateList.valueOf(Color.RED));
+                password_editText.setHint(FILL_FIELD);
+            }
+
+            Toast.makeText(this, EMPTY_FIELDS, Toast.LENGTH_SHORT).show();
         }
-
-        // Add to sharedPreferences
-        Preferences.savePreferences(this, "token", token);
-        Preferences.savePreferences(this, "expiry", expiry_date);
-        Preferences.savePreferences(this, "name", name);
-
-        // Set the Token
-        Token.setToken(token);
-        Token.setExpiry_date(expiry_date);
-        Token.setUser_name(name);
-
-        // Switch to main activity
-        Intent main_activity = new Intent(Login.this, MainActivity.class);
-        startActivity(main_activity);
-
-        // Finish the activity
-        finish();
     }
 }
